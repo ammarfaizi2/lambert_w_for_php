1a1bf22c917f27deac79cedccba8c06da5e92b0b
Ammar Faizi <ammarfaizi2@gmail.com>
2020-07-12 17:00:43 +0700

    Clean up test case

--- test.php
+++ test.php
@@ -3,25 +3,25 @@
 for ($i = 1; $i <= 100000; $i++) {
   $x = exp(lambertw(log($i)));
   $epsilon = ($x ** $x) - $i;
-  assert($epsilon < 0.000000001) or exit(1);;
+  assert($epsilon < 0.000000001) or exit(1);
 }
 
 for ($i = 1; $i <= 100000; $i++) {
   $x = exp(lambertw_fukushima(log($i)));
   $epsilon = ($x ** $x) - $i;
-  assert($epsilon < 0.000000001) or exit(1);;
+  assert($epsilon < 0.000000001) or exit(1);
 }
 
 for ($i = 1; $i <= 100000; $i++) {
   $x = exp(lambertw(log($i) * exp(log($i))));
   $epsilon = $x - $i;
-  assert($epsilon < 0.000000001) or exit(1);;
+  assert($epsilon < 0.000000001) or exit(1);
 }
 
 for ($i = 1; $i <= 100000; $i++) {
   $x = exp(lambertw_fukushima(log($i) * exp(log($i))));
   $epsilon = $x - $i;
-  assert($epsilon < 0.000000001) or exit(1);;
+  assert($epsilon < 0.000000001) or exit(1);
 }
 
 echo "All green!\n";
